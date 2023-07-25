61fbbc937a99625ff3691341c1c443cd7928a312
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-07-24 23:51:37 -0300

    config header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,17 @@
-import food from "../../images/food.jpeg";
+import { Banner1, Banner2, Banner3, Banner4, Banner5 } from "../../images";
 import { Main } from "./styles";
 
 const Header = () => {
+  const images = [Banner1, Banner2, Banner3, Banner4, Banner5];
+
   return (
     <Main>
       <div className="boxImage">
-        <img src={food} alt="Food Images" className="imageFood" />
+        <img
+          src={images[Math.floor(Math.random() * images.length)]}
+          alt="Food Images"
+          className="imageFood"
+        />
       </div>
 
       <div className="boxTitle">
