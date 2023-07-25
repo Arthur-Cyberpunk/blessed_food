595831d16589c37adbdab21dc55c0c661d3aa53e
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-07-25 12:03:58 -0300

    config routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { Outlet, Route, Routes } from "react-router-dom";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import RecipeDetail from "./pages/RecipeDetail";
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+}
 
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="recipes/:id" element={<RecipeDetail />} />
+        </Route>
+      </Routes>
+    </div>
   );
 }
 
