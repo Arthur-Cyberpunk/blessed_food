0c956354cb62b347fea09bca5300e1a1262fc0b9
Arthur Gabriel dos Santos <santos.santos86@hotmail.com>
2023-07-27 18:08:36 -0300

    config footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,5 +1,76 @@
+import { FaFacebook, FaInstagram, FaTwitter, FaYoutube } from "react-icons/fa";
+import {
+  BoxLegal,
+  BoxLinks,
+  BoxName,
+  BoxSocialMedia,
+  FooterFood,
+} from "./styles";
+
 const Footer = () => {
-  return <div></div>;
+  return (
+    <FooterFood>
+      <div>
+        <BoxName>
+          <p>
+            Flavor<span>Verse</span>
+          </p>
+        </BoxName>
+
+        <BoxLinks>
+          <p>QUICK LINKS</p>
+
+          <div>
+            <a href="#">Home</a>
+            <a href="#">About</a>
+            <a href="#">Services</a>
+            <a href="#">Contact</a>
+            <a href="#">Chiefs</a>
+          </div>
+        </BoxLinks>
+
+        <BoxLegal>
+          <p>LEGAL</p>
+          <div>
+            <p>Terms and Conditions</p>
+            <p>License Agreement</p>
+            <p>Privacy Policy</p>
+            <p>Copyright Information</p>
+            <p>Cookies Policy</p>
+          </div>
+        </BoxLegal>
+
+        <BoxSocialMedia>
+          <p>SOCIAL MEDIA</p>
+          <div>
+            <p>
+              <FaFacebook size={18} />
+            </p>
+
+            <p>
+              <FaInstagram size={18} />
+            </p>
+            <p>
+              <FaTwitter size={18} />
+            </p>
+            <p>
+              <FaYoutube size={18} />
+            </p>
+          </div>
+
+          {/* <Button
+                        title='Sign up'
+                        btnType='button'
+                        conteinerStyle='mt-10 md:block bg-transparent border border-white text-white hover:bg-white hover:text-black rounded-md min-w-[130px]'
+                    /> */}
+        </BoxSocialMedia>
+      </div>
+
+      <div className="flex items-center justify-center py-10">
+        <span className="text-gray-400 leading-10">CodeWave &copy; 2023</span>
+      </div>
+    </FooterFood>
+  );
 };
 
 export default Footer;
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,6 +1,99 @@
 import styled from "styled-components";
 
-export const Main = styled.main`
-  width: 100%;
-  height: 100vh;
+export const FooterFood = styled.footer`
+  color: #fff;
+  //padding: 5rem;
+  background: #000;
+
+  div {
+    width: 100%;
+    margin: 0 auto;
+    //padding: 5rem;
+    display: flex;
+    //flex-direction: column;
+    gap: 2rem;
+    justify-content: space-between;
+  }
+`;
+
+export const BoxName = styled.div`
+  display: flex;
+
+  p {
+    font-weight: bold;
+    text-align: center;
+
+    span {
+      color: #00ff00;
+      font-size: 1rem;
+    }
+  }
+`;
+
+export const BoxLinks = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  p {
+    font-weight: bold;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+
+    a {
+      color: #fff;
+      font-size: 1rem;
+      text-decoration: none;
+      //padding: 2rem 0;
+    }
+  }
+`;
+
+export const BoxLegal = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  p {
+    font-weight: bold;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    text-align: start;
+
+    p {
+      color: #fff;
+      font-size: 1rem;
+      text-decoration: none;
+      //padding: 2rem 0;
+    }
+  }
+`;
+
+export const BoxSocialMedia = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  p {
+    font-weight: bold;
+    text-align: center;
+  }
+
+  div {
+    display: flex;
+    //text-align: center;
+
+    p {
+      color: #fff;
+      font-size: 1rem;
+      text-decoration: none;
+      //padding: 2rem 0;
+    }
+  }
 `;
